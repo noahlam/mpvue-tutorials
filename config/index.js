8786b8bf6892b08d47cea72f61c249f78817917e
noahlam <284516433@qq.com>
2018-04-01 10:39:58 +0800

    remove moment

--- config/index.js
+++ config/index.js
@@ -23,7 +23,7 @@ module.exports = {
   },
   dev: {
     env: require('./dev.env'),
-    port: 8080,
+    port: 8888,
     // 在小程序开发者工具中不需要自动打开浏览器
     autoOpenBrowser: false,
     // assetsSubDirectory: 'static',
